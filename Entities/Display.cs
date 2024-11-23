2ee6e01ac899161b672bac9f7a9a6560a5def4fc
Gabriel Mendes <gabrielx165@gmail.com>
2024-11-23 18:52:09 -0300

    Realiza implementação inicial do display

---
+++ Entities/Display.cs
@@ -0,0 +1,61 @@
+namespace chip8.Entities;
+
+public class Display
+{
+    public int Lines { get; set; }
+    public int Columns { get; set; }
+    public int[,] Matrix { get; }
+
+    public Display(int lines, int columns)
+    {
+        Lines = lines;
+        Columns = columns;
+        Matrix = new int[lines, columns];
+    }
+
+    public void WriteSprite(int xPoint, int yPoint, byte[] sprite)
+    {
+        if(sprite.Length > 15)
+        {
+            throw new Exception("Sprite length exceded");
+        }
+        var column = xPoint;
+        var line = yPoint;
+        
+        foreach(var b in sprite)
+        {
+            // 00111000
+            for(var i = 7; i>=0; i--)
+            {
+                var mask = (byte) (1 << i);
+                var bitOn = (mask & b) != 0;
+                if(bitOn)
+                {
+                    Matrix[line, column] = Matrix[line, column] == 0 ? 1 : 0;
+                }
+
+                column++;
+            }
+            column = xPoint;
+            line++;
+        }
+    }
+
+    public void Test()
+    {
+        for(var i = 0; i < Lines; i++)
+        {
+            for (var j = 0; j < Columns; j++)
+            {
+                if (Matrix[i,j] == 1)
+                {
+                    Console.Write("■");
+                }else {
+                    Console.Write(" ");
+                }
+                
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using chip8.Entities;
+
+var memory = new Memory();
+
+var display = new Display(32, 64);
+
+// for(var i = 0; i < 32; i++)
+// {
+//     for(var j = 0; j < 64; j++)
+//     {
+//         Console.WriteLine(display.Matrix[i,j]);
+//     }
+// }
+
+// Row 1:  00111000
+// Row 2:  01111100
+// Row 3:  11111110
+// Row 4:  11111110
+// Row 5:  01111100
+// Row 6:  00111000
+// Row 7:  00010000
+// Row 8:  00000000
+
+var sprite = new byte[]
+{
+    0X38,
+    0X7C,
+    0XFE,
+    0XFE,
+    0X7C,
+    0X38,
+    0X10,
+    0X00
+};
+
+display.WriteSprite(0,0, sprite);
+
+display.Test();
